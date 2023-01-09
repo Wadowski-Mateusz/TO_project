4b7e32bdb6cc9683da9a8ad0945708b566bf7852
Wiktoria <krakowianw@gmail.com>
2023-01-09 16:03:18 +0100

    not finished

---
+++ shop/src/main/java/org/shop/classes/UserExistsAuthorization.java
@@ -0,0 +1,23 @@
+package org.shop.classes;
+
+import org.shop.app.ServerApp;
+
+public class UserExistsAuthorization extends AbstractAuthorization {
+        private Database database;
+    
+        public UserExistsAuthorization(Server server) {
+            this.database = server;
+        }
+    
+        public boolean check(String email, String password) {
+            if (!database.hasEmail(email)) {
+                System.out.println("This email is not registered.");
+                return false;
+            }
+            if (!database.isValidPassword(email, password)) {
+                System.out.println("Wrong password.");
+                return false;
+            }
+            return checkNext(email, password);
+        }
+}
