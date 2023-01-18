81e88406cfd0b1e7ed2ba44f371c3f1e7dacf703
Wiktoria <krakowianw@gmail.com>
2023-01-18 15:36:11 +0100

    comments delete

--- src/main/java/org/shop/app/ClientApp.java
+++ src/main/java/org/shop/app/ClientApp.java
@@ -156,35 +156,5 @@ public class ClientApp {
             System.exit(1);
         }
         System.out.println("Client end");
-
-//        try {
-//            // Connecting to server
-//            System.out.println("Connecting to server...");
-//            Socket socket = new Socket("localhost", 9999);
-//
-//            // Reading and writing to the connection
-//            BufferedReader myInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            PrintStream myOutput = new PrintStream(socket.getOutputStream());
-//
-//            // Sending message to the server
-//            myOutput.println("Client message");
-//
-//            // Checking server respond
-//            String buf = myInput.readLine();
-//            if(buf != null)
-//                System.out.println("Server respond: " + buf);
-//
-//            // Closing the connection
-//            myOutput.close();
-//            socket.close();
-//            System.out.println("Client end");
-//
-//        } catch (ConnectException e){
-//            System.out.println("Server is offline");
-//            System.exit(404);
-//        } catch (IOException e){
-//            e.printStackTrace();
-//            System.out.println("Error");
-//        }
     }
 }
--- src/main/java/org/shop/app/ServerApp.java
+++ src/main/java/org/shop/app/ServerApp.java
@@ -19,36 +19,5 @@ public class ServerApp {
             Socket socket = serverSocket.accept();
             new ServerAppThread(socket).start();
         }
-
-//        try {
-//            // Creation of a server socket on port 7070
-//            ServerSocket serverSocket = new ServerSocket(9999);
-//            System.out.println("host: "  + InetAddress.getLocalHost());
-//            System.out.println("port: "  + serverSocket.getLocalPort());
-//
-//            // Connection
-//            Socket socket = serverSocket.accept();
-//
-//            // Reading and writing to the connection
-//            BufferedReader myInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            PrintStream myOutput = new PrintStream(socket.getOutputStream());
-//
-//            // Read from input stream
-//            String buf = myInput.readLine();
-//            if (buf != null){
-//                System.out.println("buf: " + buf);
-//                myOutput.print("received");
-//            }
-//
-//            // Closing the connection
-//            socket.close();
-//            System.out.println("Server close");
-//
-//        } catch (IOException e){
-//            e.printStackTrace();
-//            System.out.println("Error");
-//        }
-
-
     }
 }
